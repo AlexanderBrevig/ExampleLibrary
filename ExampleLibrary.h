84dd5c98cb3d9559895120f58a8e4145a7af60a4
Brett Hagman <bhagman@roguerobotics.com>
2013-08-09 16:42:26 -0400

    Initial Import

---
+++ ExampleLibrary.h
@@ -0,0 +1,58 @@
+/*
+|| @author         Brett Hagman <bhagman@wiring.org.co>
+|| @url            http://wiring.org.co/
+||
+|| @description
+|| | Example Wiring Library
+|| |
+|| | This is a complete example of a Wiring Library
+|| #
+||
+|| @license Please see the accompanying LICENSE file for this project.
+||
+*/
+
+#ifndef EXAMPLELIBRARY_H
+#define EXAMPLELIBRARY_H
+
+/*
+|| ExampleLibrary version
+|| @Version 1.0.0
+*/
+
+// Version below is 1 00 00 (integer representation of above)
+#define EXAMPLELIBRARY_VERSION          10000
+
+
+/*
+|| Public Constants
+*/
+
+#define FORWARD                         1
+#define BACKWARD                        -1
+
+
+/*
+|| Class
+*/
+
+class ExampleLibrary
+{
+  public:
+    // Constructor
+    ExampleLibrary(int direction);
+
+    // Methods
+    void doThatThing();
+
+  private:
+    // Methods
+    void setIncrement(int value);
+
+    // Members
+    int increment;
+    int total;
+};
+
+
+#endif
